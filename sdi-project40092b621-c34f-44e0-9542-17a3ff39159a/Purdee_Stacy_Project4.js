18e6a641f9a5aa84b1238d9c4aef073b1d7a007d
Stacy Purdee <stacy.purdee@gmail.com>
2013-10-24 14:55:10 -0400

    1

---
+++ sdi-project40092b621-c34f-44e0-9542-17a3ff39159a/Purdee_Stacy_Project4.js
@@ -0,0 +1,32 @@
+//alert("JavaScript works!");
+
+//Stacy Purdee
+//SDI 1310
+//October 24th 2013
+//Wk 4 Library
+
+var myLibrary(){
+    
+
+    
+//#1
+//Given a string that is a list of things seperated by a given string,
+//as well as another string seperator changed to the second: "a,b,c" + "," + "/" to "a/b/c"
+
+var stringSeparator= function(changeString) {
+    if (changeString) {
+        return console.log("a/" + "b/" + "c/");
+    }
+}
+stringSeparator("a,b,c");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
